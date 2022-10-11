1a946e210a3f33628b7500302c00d7b28300659d
Imam Hossain <imam.hossain@brainstation-23.com>
2022-10-11 10:46:58 +0600

    sql file update

--- Database/studymate.sql
+++ Database/studymate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2022 at 02:18 PM
+-- Generation Time: Oct 11, 2022 at 06:43 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -36,10 +36,13 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`name`) VALUES
-('Bangla'),
-('Math'),
+('Computer Programming'),
+('Networking'),
 ('English Novel'),
-('Programming');
+('Mathematics'),
+('Economics'),
+('Data Structure'),
+('Algorithms');
 
 -- --------------------------------------------------------
 
@@ -58,13 +61,6 @@ CREATE TABLE `conversations` (
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `conversations`
---
-
-INSERT INTO `conversations` (`id`, `postId`, `posterUserId`, `accepterUserId`, `receiverUserId`, `pendingMsgUserId`, `isSuccess`, `createdAt`) VALUES
-(1, 2, 1, 2, 1, NULL, 1, '2022-10-10 12:07:07');
-
 -- --------------------------------------------------------
 
 --
@@ -79,16 +75,6 @@ CREATE TABLE `messages` (
   `userName` varchar(100) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`id`, `conversationId`, `message`, `userId`, `userName`, `createdAt`) VALUES
-(1, 1, 'test message', 1, 'user one', '2022-10-10 12:10:45'),
-(2, 1, 'test', 1, 'user one', '2022-10-10 12:15:13'),
-(3, 1, 'tets', 2, 'user two', '2022-10-10 12:15:19'),
-(4, 1, 'test', 2, 'user two', '2022-10-10 12:15:40');
 
 -- --------------------------------------------------------
 
@@ -109,14 +95,6 @@ CREATE TABLE `posts` (
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `title`, `writerName`, `description`, `tag`, `type`, `isSuccess`, `userId`, `photo`, `createdAt`) VALUES
-(2, 'test post', 'test writer', 'test description', 'Bangla', 'Request', 1, 1, 0x2e2f696d616765732f3831323435343232332e706e67, '2022-10-10 11:27:40'),
-(3, 'book offer', 'Mr. Xsdfds', 'sdfsdaf', 'Programming', 'Offer', 0, 2, 0x2e2e2f696d672f64656d6f5f626f6f6b2e737667, '2022-10-10 12:12:05');
-
 -- --------------------------------------------------------
 
 --
@@ -136,14 +114,6 @@ CREATE TABLE `users` (
   `photo` longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `address`, `phone`, `point`, `password`, `department`, `roll`, `photo`) VALUES
-(1, 'user one', 'userone@gmail.com', 'user one address', '01763183408', 1, 'userone', 'ice', 'ASH1611029M', 0x696d616765732f3536353537383430312e706e67),
-(2, 'user two', 'usertwo@gmail.com', 'user two address', '01763183401', 3, 'usertwo', 'eee', 'ASH1611022M', 0x2e2f696d616765732f3530313530363734312e706e67);
-
 -- --------------------------------------------------------
 
 --
@@ -155,14 +125,6 @@ CREATE TABLE `user_category` (
   `userId` int(11) NOT NULL,
   `category` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_category`
---
-
-INSERT INTO `user_category` (`id`, `userId`, `category`) VALUES
-(4, 1, 'Math'),
-(5, 1, 'English Novel');
 
 --
 -- Indexes for dumped tables
@@ -206,31 +168,31 @@ ALTER TABLE `user_category`
 -- AUTO_INCREMENT for table `conversations`
 --
 ALTER TABLE `conversations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT for table `user_category`
 --
 ALTER TABLE `user_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
